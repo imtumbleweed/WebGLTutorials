73417a7f96d82e8d7815cfeca7f9cb606ab79929
Greg Sidelnikov <greg.sidelnikov@gmail.com>
2017-05-22 19:14:33 -0400

    Added camera placeholder class

---
+++ camera.js
@@ -0,0 +1,17 @@
+// Main camera object class
+// Note: The Camera class is dependent on matrix-generation functions located in matrix.js
+var Camera = function()
+{
+    this.x = 0;
+    this.y = 0;
+    this.z = 0;
+
+    // This camera represented by a "mat4" matrix
+    this.ViewMatrix = null;
+
+    // Create LookAt matrix
+    this.LookAt(tx, ty, tz)
+    {
+
+    }
+}
