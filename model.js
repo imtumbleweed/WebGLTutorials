54b94fbe50ce6b13dfcc3d6ba2a6c5eb4b46d09b
Greg Sidelnikov <greg.sidelnikov@gmail.com>
2017-05-24 20:34:39 -0400

    Enabled normal vector

--- model.js
+++ model.js
@@ -10,6 +10,7 @@ function ModelData() {
     this.vertexbuffer = null; //gl.createBuffer();
     this.colorbuffer = null; //gl.createBuffer();
     this.texturebuffer = null; //gl.createBuffer();
+    this.normalbuffer = null; //gl.createBuffer();
     this.indexbuffer = null; //gl.createBuffer();
 }
 
@@ -33,7 +34,7 @@ function BindModel(model_ID)
     var vertices = window.ref_arrayMDL[i][0]; // Get vertex data from loaded model
     var colors = window.ref_arrayMDL[i][1];
     var uvs = window.ref_arrayMDL[i][2];
-    var normals = window.ref_arrayMDL[i][3]; // unused
+    var normals = window.ref_arrayMDL[i][3];
     var indices = window.ref_arrayMDL[i][4];
 
     // set global pointer to this model's indices
@@ -42,6 +43,7 @@ function BindModel(model_ID)
     if (Model[i].vertexbuffer == null) Model[i].vertexbuffer = gl.createBuffer();
     if (Model[i].colorbuffer == null) Model[i].colorbuffer = gl.createBuffer();
     if (Model[i].texturebuffer == null) Model[i].texturebuffer = gl.createBuffer();
+    if (Model[i].normalbuffer == null) Model[i].normalbuffer = gl.createBuffer();
     if (Model[i].indexbuffer == null) Model[i].indexbuffer = gl.createBuffer();
 
     var BYTESIZE = vertices.BYTES_PER_ELEMENT;
@@ -50,7 +52,7 @@ function BindModel(model_ID)
     gl.bindBuffer(gl.ARRAY_BUFFER, Model[i].vertexbuffer);
     // Send our vertex data to the buffer using floating point array
     gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-    var coords = gl.getAttribLocation(Shader.textureMapProgram, "a_Position");
+    var coords = gl.getAttribLocation(Shader.directionalProgram, "a_Position");
     gl.vertexAttribPointer(coords, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(coords); // Enable it
     // We're done; now we have to unbind the buffer
@@ -60,7 +62,7 @@ function BindModel(model_ID)
     gl.bindBuffer(gl.ARRAY_BUFFER, Model[i].colorbuffer);
     // Send our vertex data to the buffer using floating point array
     gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
-    var col = gl.getAttribLocation(Shader.textureMapProgram, "a_Color");
+    var col = gl.getAttribLocation(Shader.directionalProgram, "a_Color");
     gl.vertexAttribPointer(col, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(col); // Enable it
     // We're done; now we have to unbind the buffer
@@ -70,10 +72,20 @@ function BindModel(model_ID)
     gl.bindBuffer(gl.ARRAY_BUFFER, Model[i].texturebuffer);
     // Send our texture image data to the buffer using floating point array
     gl.bufferData(gl.ARRAY_BUFFER, uvs, gl.STATIC_DRAW);
-    var uv = gl.getAttribLocation(Shader.textureMapProgram, "a_Texture");
+    var uv = gl.getAttribLocation(Shader.directionalProgram, "a_Texture");
     gl.vertexAttribPointer(uv, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(uv); // Enable it
     // We're done; now we have to unbind the buffer (optional but probably a good idea)
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    // Bind normalbuffer to ARRAY_BUFFER
+    gl.bindBuffer(gl.ARRAY_BUFFER, Model[i].normalbuffer);
+    // Send our vertex data to the buffer using floating point array
+    gl.bufferData(gl.ARRAY_BUFFER, normals, gl.STATIC_DRAW);
+    var n = gl.getAttribLocation(Shader.directionalProgram, "a_Normal");
+    gl.vertexAttribPointer(n, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(n); // Enable it
+    // We're done; now we have to unbind the buffer
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
     // Bind indices to ELEMENT_ARRAY_BUFFER
